850ca7afd69a2b9b9fce78dba562d1c349f01c2d
Eduard Ghenea <87914375+Edi013@users.noreply.github.com>
2024-03-15 18:49:26 +0200

    Bind fn need an uuid

--- infrastructure/src/repositories/item_repository.rs
+++ infrastructure/src/repositories/item_repository.rs
@@ -1,33 +1,32 @@
+use crate::models::item::ItemModel;
+use crate::repositories::DatabaseRepositoryImpl;
 use anyhow::anyhow;
 use async_trait::async_trait;
 use domain::app_error::AppError;
 use domain::entities::item::Item;
 use domain::entities::user::User;
 use domain::id::Id;
-use crate::models::item::ItemModel;
-use crate::repositories::DatabaseRepositoryImpl;
 use domain::interfaces::i_item_repository::IItemRepository;
-use log::info;
+use log::{error, info};
+use sqlx::types::Uuid;
 
 #[async_trait]
-impl IItemRepository for DatabaseRepositoryImpl<Item>{
-    async fn get_all_by_user_id(&self, user_id: String) -> anyhow::Result<Option<Vec<Item>>> {
+impl IItemRepository for DatabaseRepositoryImpl<Item> {
+    async fn get_all_by_user_id(&self, user_id: String) -> anyhow::Result<Vec<Item>> {
         let pool = self.pool.0.clone();
         let result = sqlx::query_as::<_, ItemModel>("SELECT * FROM items WHERE user_id = $1")
-            .bind(user_id)
+            .bind(Uuid::parse_str(user_id.to_string().as_str()).map_err(|e| anyhow!("{:?}", e))?)
             .fetch_all(pool.as_ref())
             .await
-            .map_err(|e| anyhow!("{:?}", e))?;
+            .map_err(|e| {
+                error!("{:?}", e);
+                anyhow!("{:?}", e)
+            })?;
 
-        match result {
-            _items => {
-                let mut items: Vec<Item> = vec![];
-                for i in _items{
-                    items.push(Item::try_from(i).unwrap());
-                }
-                Ok(Some(items))
-            },
-            _ => Ok(None),
-        }
+        let items = result
+            .into_iter()
+            .map(|item_model| Item::try_from(item_model))
+            .collect();
+        items
     }
 }
